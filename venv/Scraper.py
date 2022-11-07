8d8eaddc344b1f8fe1e1086dd8da7da6d6269559
Karim Abdul Ulmann <119329701@umail.ucc.ie>
2022-11-07 23:39:20 +0000

    replaced hard coded variables with env vars

--- venv/Scraper.py
+++ venv/Scraper.py
@@ -6,6 +6,9 @@ from bs4 import BeautifulSoup
 import smtplib
 from datetime import datetime
 import time
+import os
+from dotenv import load_dotenv
+
 
 # URL = 'https://www.amazon.de/MWP22ZM-A-Apple-AirPods-Pro/dp/B07ZPML7NP/ref=sr_1_1_sspa?dchild=1&keywords=airpods+pro&qid=1629404730&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyQlk3MDFETUdIMjhVJmVuY3J5cHRlZElkPUEwMDAzNjg1M0dSM09RTU9HQlA0QyZlbmNyeXB0ZWRBZElkPUEwMjkzODc2QzFKTVRZVUxMSjRQJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=='
 # headers = {
@@ -14,6 +17,10 @@ import time
 
 class WebParser:
 
+    load_dotenv()
+    SENDER_EMAIL = os.getenv("SENDER_EMAIL")
+    SENDER_PASSWORD = os.getenv("SENDER_PASSWORD")
+
     headers = {
         "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36'}
 
@@ -21,16 +28,22 @@ class WebParser:
         self._URL = URL
         self._emailReceiver = emailReceiver
 
+    def getEmailReceiver(self):
+        return self._emailReceiver
+
+    def getURL(self):
+        return self._URL
+
 
     def getTitle(self):
-        page = requests.get(self._URL, headers=headers)
+        page = requests.get(self.getURL, headers=headers)
         soup = BeautifulSoup(page.content, 'html.parser')
         title = soup.find(id='productTitle').get_text()
         return title.strip()
 
 
     def getPrice(self):
-        page = requests.get(URL, headers=headers)
+        page = requests.get(self.getURL(), headers=headers)
         soup = BeautifulSoup(page.content, 'html.parser')
         price = soup.find(id='priceblock_ourprice').get_text()
         converted_price = float(price[0:5].replace(',', '.'))
@@ -71,60 +84,53 @@ class WebParser:
             nf.write('Title: ' + title.strip() + '\n  Current Price: ' + str(
                 price).strip() + '\n logged at ' + getTime() + '\n \n')
             nf.close()
-            send_mailGood()
+            self.compose_positive_mail()
         elif price == readPrice:
             # no change
-            send_mailNoChange()
+            self.compose_negative_mail()
 
 
-    def send_mailGood(self, recipient):
-        # receiverEmail = getInfo()
+    def senderLogin(self):
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.ehlo()
         server.starttls()
         server.ehlo()
 
-        server.login('dummymagician22@gmail.com', 'itewskcoplhsmjyg') # fix this
+        server.login(SENDER_EMAIL, SENDER_PASSWORD)
+
+    def sendMail(self, subject, body,recepient):
+        self.senderLogin()
+        msg = f"Subject: {subject}\n\n{body}"
+        server.sendmail(
+            SENDER_EMAIL,
+            recepient,
+            msg
+        )
+        server.quit()
+
+    def compose_positive_mail(self):
+
+        self.senderLogin()
 
         subject = 'PRICE CHANGE ' + getTitle() + '!  ' + str(getPrice()) + 'EUR at ' + str(getDate())
         body = 'CURRENT PRICE IS NOW ' + str(
             getPrice()) + 'EUR as of ' + getTime() + ' ' + str(
             getDate()) + '\nCheck the the product linked below for details! \n\n' + URL
 
-        msg = f"Subject: {subject}\n\n{body}"
-
-        server.sendmail(
-            'dummymagician22@gmail.com',
-            recipient,
-            msg
-        )
-        print('email sent!')
-        server.quit()
+        self.sendMail(subject,body,self.getEmailReceiver())
 
 
-    def send_mailNoChange(self, recepient):
-        # receiverEmail = getInfo()
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.ehlo()
-        server.starttls()
-        server.ehlo()
+    def compose_negative_mail(self):
 
-        server.login('dummymagician22@gmail.com', 'itewskcoplhsmjyg')
+        self.senderLogin()
+
         subject = 'No change on ' + getTitle() + '!  ' + str(getPrice()) + 'EUR at ' + str(getDate())
         body = 'Price remains at ' + str(
             getPrice()) + 'EUR as of ' + getTime() + ' ' + str(
             getDate()) + '\nCheck the the product linked below for details! \n\n' + URL
 
-        msg = f"Subject: {subject}\n\n{body}"
-        server.sendmail(
-            'dummymagician22@gmail.com',
-            recepient,
-            msg
-        )
-        print('email sent!')
-        server.quit()
+        self.sendMail(subject, body, self.getEmailReceiver())
 
-        # here is a test
 
 
     # while True:
